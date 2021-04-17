9e1ebfb2116d1d2a8989c6abb4e612c147490153
Ubuntu <andrefiliperamalho1990@gmail.com>
2021-04-17 15:42:07 +0100

    changed monthly total calculation on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
   def index
-      @monthly_total = Booking.find_by_sql("SELECT count(distinct d.id) as num_dogs, sum((end_date - start_date)) as num_days, sum(price * (end_date - start_date)) as price FROM
-                                                    bookings b join dogs d on b.dog_id = d.id
+      @monthly_total = Booking.find_by_sql("SELECT count(distinct q.id) as num_dogs, sum((q.end_date - q.start_date)) as num_days,sum(q.price) as price FROM
+                                              ( SELECT d.id as id, start_date, end_date, case when changed_price is null or changed_price=0 then (price * (end_date - start_date)) else changed_price end as price
+                                                   from bookings b join dogs d on b.dog_id = d.id
                                                         where EXTRACT(MONTH from end_date) = EXTRACT(MONTH from NOW())
-                                                            and EXTRACT(YEAR from end_date) = EXTRACT(YEAR from NOW())")
+                                                            and EXTRACT(YEAR from end_date) = EXTRACT(YEAR from NOW()) ) as q")
 
       @next_bookings = Booking.find_by_sql("SELECT b.id, d.name, d.ownername, d.contact, b.start_date, b.end_date, b.price, (end_date - start_date) as num_nights, (b.price * (end_date - start_date)) as total
                                                from bookings b join dogs d on dog_id = d.id
