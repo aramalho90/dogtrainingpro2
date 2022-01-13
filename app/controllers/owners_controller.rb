9a51c0de84e9a0dc53ef3de6bc04e69679586e0f
Ubuntu <andrefiliperamalho1990@gmail.com>
2022-01-13 21:10:16 +0000

    owner pages created

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -25,7 +25,7 @@ class OwnersController < ApplicationController
 
     respond_to do |format|
       if @owner.save
-        format.html { redirect_to owner_url(@owner), notice: "Owner was successfully created." }
+        format.html { redirect_to owner_url(@owner) }
         format.json { render :show, status: :created, location: @owner }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class OwnersController < ApplicationController
   def update
     respond_to do |format|
       if @owner.update(owner_params)
-        format.html { redirect_to owner_url(@owner), notice: "Owner was successfully updated." }
+        format.html { redirect_to owner_url(@owner)}
         format.json { render :show, status: :ok, location: @owner }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class OwnersController < ApplicationController
     @owner.destroy
 
     respond_to do |format|
-      format.html { redirect_to owners_url, notice: "Owner was successfully destroyed." }
+      format.html { redirect_to owners_url}
       format.json { head :no_content }
     end
   end
