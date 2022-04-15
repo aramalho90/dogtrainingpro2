99625dc48fcf829be93cbfbe81bddadbde9bf231
Ubuntu <andrefiliperamalho1990@gmail.com>
2022-04-15 16:42:43 +0100

    added vacancies to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,15 @@
 class HomeController < ApplicationController
+
+    def index
+        @vacanciesG = Grouptrain.find_by_sql("select m.id, time, \"limit\", count(1) filter (where status in ('A decorrer','A realizar aulas teóricas')) from
+            groupmaps m left join grouptrains t
+            on m.id = t.groupmap_id
+            group by m.id, time, \"limit\"
+            order by m.id ")
+
+        @vacanciesP = Pttrain.find_by_sql("select m.id, day, \"limit\", count(1) filter (where status in ('A decorrer','A realizar aulas teóricas')) from
+            ptmaps m left join pttrains t
+            on m.id = t.ptmap_id
+            group by m.id, day, \"limit\"
+            order by m.id ")    end
 end
